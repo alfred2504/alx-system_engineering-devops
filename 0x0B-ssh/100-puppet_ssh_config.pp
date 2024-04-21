a90cc955f6c77fb13c82d60949c5708d988ac24c
alfred2504 <inforamaiv@gmail.com>
2024-04-21 13:42:27 -0700

    ssh  project

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,20 @@
+# Using Puppet to set client authentication method
+file { '/etc/ssh/ssh_config':
+  ensure => present,
+}
+
+# Ensure IdentityFile line is present and has the correct value
+file_line { 'identity_file':
+  path    => '/etc/ssh/ssh_config',
+  line    => 'IdentityFile ~/.ssh/school',
+  match   => '^IdentityFile',
+  replace => true
+}
+
+# Ensure PasswordAuthentication line is present and has the correct value
+file_line { 'password_authentication':
+  path    => '/etc/ssh/ssh_config',
+  line    => 'PasswordAuthentication no',
+  match   => '^PasswordAuthentication',
+  replace => true
+}
